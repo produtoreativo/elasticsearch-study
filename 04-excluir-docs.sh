d546e56d0f669dae59b11d7c0be850dff0b43549
Christiano Milfont <cmilfont@gmail.com>
2024-08-10 11:06:21 -0300

    corrigindo o script de deletar

--- 04-excluir-docs.sh
+++ 04-excluir-docs.sh
@@ -7,7 +7,7 @@ ELASTICSEARCH_URL="http://localhost:9200"
 INDEX_NAME="products"
 
 # Delete all documents from the index
-curl -X DELETE "$ELASTICSEARCH_URL/$INDEX_NAME/_delete_by_query" -H 'Content-Type: application/json' -d '{
+curl -X POST "$ELASTICSEARCH_URL/$INDEX_NAME/_delete_by_query" -H 'Content-Type: application/json' -d '{
   "query": {
     "match_all": {}
   }
